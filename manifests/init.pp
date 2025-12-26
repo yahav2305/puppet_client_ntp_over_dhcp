51bf189c0eb5ef9fda92f08c334dde9de563dd39
yahav2305 <15077067+yahav2305@users.noreply.github.com>
2025-12-26 18:29:13 +0200

    Converted puppet code to use style guide

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,26 +1,26 @@
+# Configures a Linux client to use an NTP server published over DHCP
 class clients_ntp_configuration {
-
-  file { [ '/etc/systemd/timesyncd.conf.d' ]:
+  file { ['/etc/systemd/timesyncd.conf.d']:
     ensure => directory,
-    owner => 'root',
-    group => 'root',
-    mode => '0755',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
   }
-  -> file { [ '/etc/NetworkManager', '/etc/NetworkManager/dispatcher.d' ]:
+  -> file { ['/etc/NetworkManager', '/etc/NetworkManager/dispatcher.d']:
     ensure => directory,
-    owner => 'root',
-    group => 'root',
-    mode => '0755',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
   }
 
   file { '/etc/NetworkManager/dispatcher.d/10-update-timesyncd':
-    ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0755',
-    source => 'puppet:///modules/clients_ntp_configuration/10-update-timesyncd',
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    source  => 'puppet:///modules/clients_ntp_configuration/10-update-timesyncd',
     require => File['/etc/NetworkManager/dispatcher.d'],
-    notify => Exec['Restart NetworkManager'],
+    notify  => Exec['Restart NetworkManager'],
   }
 
   exec { 'Restart NetworkManager':
